bc1fe33089e9161a7a084e8bdc35b5fea03d5ed9
Petrova <amandamodzinski@hotmail.com>
2024-11-25 22:25:08 -0300

    Add files via upload

---
+++ peojeto_c_02/projeto_c_02_paramentro_por_valor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int variavelGlobal;
+
+void imprimirNumero(){
+	printf(%d, variavelGlobal);	
+}
+
+float converterKgParaGrama(float peso){
+	peso = peso * 1000;
+	return peso;
+}
+
+int main (){
+	float p = 10.0;
+	
+	printf("Peso antes de chamar a função %f\n", p);
+	float res = converterKgParaGrama(p);
+	printf("Peso depois de chamar a função %f\n", p);
+	print("Resultado da função é %f\n", res);
+	
+	return 0;
+}
